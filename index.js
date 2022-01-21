722ec2dac507f31f367d5d7528f5a250488def95
Tammy <tammycollinse@gmail.com>
2022-01-21 10:36:37 -0500

    commit tree 9a3e09e50d1454efd19f29c16360214183ea3a4d

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Follow along with the examples here
+//function sayHello() {
+  //  console.log("Hello");
+//}
+//sayHello();
+
+//function sayHelloGuadalupe() {
+  //  console.log("Hello Guadalupe!");
+//}
+//sayHelloGuadalupe();
+
+//function sayHelloToLiz() {
+  //  console.log("Hello Liz!");
+//}
+//sayHelloToLiz(); 
+
+//function sayHelloToSamip() {
+  // console.log("Hello Samip!");
+//}
+//sayHelloToSamip();
+
+function say(greeting, firstName) {
+    console.log("greeting: ", greeting);
+    console.log("First name: ", firstName);
+    console.log(`${greeting}, ${firstName}`);
+}
+say("Hello ", "Julio");
+
+//function add(x , y){
+  //console.log(add(1, 2));
+    
+//}
+// add();
+
+
+
